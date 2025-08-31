24c1df0064e40b42f72923055fecc140ce8a3302
Hassan <hassansheikh993@gmail.com>
2025-08-31 18:20:02 +0500

    update server to set up cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { mongoConnection } from "./config/db.js";
 import { router } from "./routes/userRouts.js";
+import { doctorRouter } from "./routes/doctorRouts.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -9,10 +17,21 @@ dotenv.config();
 
 const port = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser())
+
+
+
+app.use('/uploadProfileImages', express.static(path.join(__dirname, 'public', 'uploadProfileImages')));
 
 app.use("/api",router)
+app.use("/api",doctorRouter);
 
 app.get("/",(req,res)=>{
     res.send("testing")
