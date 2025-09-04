01c899c365c6a461adcd147e19c32d3d0aa2c299
Hassan <hassansheikh993@gmail.com>
2025-09-04 20:50:38 +0500

    Added model for shared reports

--- server/model/sharedReportModel.js
+++ server/model/sharedReportModel.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const sharedReportModelSchema = mongoose.Schema({
-   report_id:{type:String},
-   patient_email:{type:String},
+   report_id:{ type: mongoose.Schema.Types.ObjectId, ref: "report", required: true },
+   patient_id:{ type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
    doctor_email:{type:String},
    viewedByDoctor: { type: Boolean, default: false }
 },{ timestamps: true })
+
+export const SharedReport = mongoose.model("sharedReport",sharedReportModelSchema);
