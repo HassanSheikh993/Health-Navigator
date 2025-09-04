5b094892ddb5a98b4db79c8f9f9b8b4053f47f01
Hassan <hassansheikh993@gmail.com>
2025-09-04 20:52:38 +0500

    Added endpoits fro uploadReport displayAllReports sendReportToDoctor

--- server/routes/reportRouts.js
+++ server/routes/reportRouts.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { uploadReport } from "../controller/reportController.js";
+import { displayAllReports, sendReportToDoctor, uploadReport } from "../controller/reportController.js";
 import { auth } from "../middleWares/authMiddleware.js";
 import { uploadMedicalReport } from "../middleWares/uploadMedicalReport.js";
 
 export const reportRouter = express.Router();
 
-reportRouter.post("/upload-report", uploadMedicalReport.single("report"), uploadReport);
-
+reportRouter.post("/upload-report",auth, uploadMedicalReport.single("report"), uploadReport);
+reportRouter.get("/allReports",displayAllReports)
+reportRouter.post("/sendReports",sendReportToDoctor)
 
