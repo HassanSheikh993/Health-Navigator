0b7730f5dac33e987bda08fa3cfbca847560a761
Hassan <hassansheikh993@gmail.com>
2025-09-05 17:49:09 +0500

    update to add allReports and getDoctorReports endpoints

--- server/routes/reportRouts.js
+++ server/routes/reportRouts.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { displayAllReports, sendReportToDoctor, uploadReport } from "../controller/reportController.js";
+import { displayReports, getAllReportsForDoctor, sendReportToDoctor, uploadReport } from "../controller/reportController.js";
 import { auth } from "../middleWares/authMiddleware.js";
 import { uploadMedicalReport } from "../middleWares/uploadMedicalReport.js";
 
 export const reportRouter = express.Router();
 
 reportRouter.post("/upload-report",auth, uploadMedicalReport.single("report"), uploadReport);
-reportRouter.get("/allReports",displayAllReports)
+reportRouter.get("/allReports",displayReports)
 reportRouter.post("/sendReports",sendReportToDoctor)
+reportRouter.get("/getDoctorReports",getAllReportsForDoctor)
 
