00b2b1bcb003785d8b5b9b557f8babd1936fd70c
Hassan <hassansheikh993@gmail.com>
2025-09-06 14:49:52 +0500

    update sharedReport model with new field: doctor_review

--- server/model/sharedReportModel.js
+++ server/model/sharedReportModel.js
@@ -4,7 +4,8 @@ const sharedReportModelSchema = mongoose.Schema({
    report_id:{ type: mongoose.Schema.Types.ObjectId, ref: "report", required: true },
    patient_id:{ type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
    doctor_email:{type:String},
-   viewedByDoctor: { type: Boolean, default: false }
+   viewedByDoctor: { type: Boolean, default: false },
+   doctor_review: { type: String, default: "" }
 },{ timestamps: true })
 
 export const SharedReport = mongoose.model("sharedReport",sharedReportModelSchema);
