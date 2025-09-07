85fcc490aff3e832fb535cbb0b10d1e9eb11af2d
Hassan <hassansheikh993@gmail.com>
2025-09-07 15:26:59 +0500

    updated sharedReport model with : doctor_review + doctor_reviewedAt fields

--- server/model/sharedReportModel.js
+++ server/model/sharedReportModel.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const sharedReportModelSchema = mongoose.Schema({
    report_id:{ type: mongoose.Schema.Types.ObjectId, ref: "report", required: true },
    patient_id:{ type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-   doctor_email:{type:String},
+   doctor_id:{ type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
    viewedByDoctor: { type: Boolean, default: false },
-   doctor_review: { type: String, default: "" }
+   doctor_review: { type: String, default: "" },
+   doctor_reviewedAt: { type: Date } 
 },{ timestamps: true })
 
 export const SharedReport = mongoose.model("sharedReport",sharedReportModelSchema);
