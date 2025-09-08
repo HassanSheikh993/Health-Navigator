c244fed5a3eb56ddd329a726c2d70826250b4dfb
Hassan <hassansheikh993@gmail.com>
2025-09-08 18:28:24 +0500

    Added 2 endpoints doctorReviewHistory and deleteUserReport

--- server/routes/reportRouts.js
+++ server/routes/reportRouts.js
@@ -1,14 +1,18 @@
 import express from "express";
-import { addDoctorReview, displayReports, getAllReportsForDoctor, getReportStats, sendReportToDoctor, uploadReport } from "../controller/reportController.js";
+import { addDoctorReview, deleteUserReport, displayReports, doctorReviewHistory, getAllReportsForDoctor, getReportStats, sendReportToDoctor, uploadReport } from "../controller/reportController.js";
 import { auth } from "../middleWares/authMiddleware.js";
 import { uploadMedicalReport } from "../middleWares/uploadMedicalReport.js";
 
 export const reportRouter = express.Router();
 
 reportRouter.post("/upload-report",auth, uploadMedicalReport.single("report"), uploadReport);
-reportRouter.get("/allReports",displayReports)
+reportRouter.get("/allReports",auth,displayReports)
 reportRouter.post("/sendReports",sendReportToDoctor)
 reportRouter.get("/getDoctorReports",auth,getAllReportsForDoctor)
 reportRouter.put("/addDoctorReview",auth,addDoctorReview)
 reportRouter.get("/getReportStats",auth,getReportStats)
+
+reportRouter.get("/doctorReviewHistory",auth,doctorReviewHistory)
+
+reportRouter.delete("/deleteUserReport",auth,deleteUserReport);
 
